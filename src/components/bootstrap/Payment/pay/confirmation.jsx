d8bd1d00a905a8ba8745b98706b3c4c4c92b829f
Essy116 <githinjie40@gmail.com>
2025-06-16 13:35:54 +0300

    {FEAT} Add MPESA payment confirmation page

---
+++ src/components/bootstrap/Payment/pay/confirmation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Confirmation = () => {
+  return (
+    <div className="bs">
+      <div>Payment Confirmed</div>
+    </div>
+  );
+};
+
+export default Confirmation;
