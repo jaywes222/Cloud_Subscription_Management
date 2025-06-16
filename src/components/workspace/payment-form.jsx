744475f6d3d884004b558d1790bfec3deb09c2bc
Essy116 <githinjie40@gmail.com>
2025-06-16 23:24:35 +0300

    {FEAT} Add Stk push and Bank Instruction page instruction page

--- src/components/workspace/payment-form.jsx
+++ src/components/workspace/payment-form.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import MpesaInstructions from "../bootstrap/Payment/text/MpesaInstructions";
-
+import Stk from "../bootstrap/Payment/text/Stk";
+import BankInstructions from "../bootstrap/Payment/pay/BankInstructions";
 const PaymentForm = () => {
   const [selectedOption, setSelectedOption] = useState("mpesa");
+
   const handleRadioClick = (value) => {
     setSelectedOption((prev) => (prev === value ? "" : value));
   };
@@ -10,7 +12,7 @@ const PaymentForm = () => {
     <div className="bs ">
       <div className="payment-container">
         <h6 className="form-title">Complete payment</h6>
-
+        <h5 className="form-title">Choose Payment method</h5>
         <form>
           <div className="radio-group">
             <label className="custom-radio">
@@ -24,6 +26,9 @@ const PaymentForm = () => {
               <span className="radio-label">MPESA</span>
             </label>
             {selectedOption === "mpesa" && <MpesaInstructions />}
+            {/* {selectedOption === "stk" && (
+              <Stk setSelectedOption={setSelectedOption} />
+            )} */}
             <label className="custom-radio">
               <input
                 type="radio"
@@ -34,6 +39,7 @@ const PaymentForm = () => {
               />
               <span className="radio-label">BANK</span>
             </label>
+            {selectedOption === "bank" && <BankInstructions />}
           </div>
         </form>
       </div>
