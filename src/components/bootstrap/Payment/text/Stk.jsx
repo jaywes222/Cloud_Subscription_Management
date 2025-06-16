acbab3fb1bf152ed92db85336f38c44866c92ffd
Essy116 <githinjie40@gmail.com>
2025-06-16 13:35:09 +0300

    {FEAT} Add MPESA STK Push payment page and flow

---
+++ src/components/bootstrap/Payment/text/Stk.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Stk = () => {
+  return (
+    <div className="bs">
+      <div className="container-fluid">
+        <div className="center-content">
+          <div className="payment-box">
+            <h6 className="form-title">STK Push Payment</h6>
+            <p>
+              Please use the STK push option on your mobile device to complete
+              the payment.
+            </p>
+            <p>Follow the instructions on your phone to proceed.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stk;
