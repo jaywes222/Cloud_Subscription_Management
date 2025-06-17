ba3ff665972a9fd6ab46b2e2ac47c5fe23c1a97b
mikewanjero <mikewanj@gmail.com>
2025-06-18 00:19:40 +0300

    {FEAT} Activate Account `logic`

--- src/components/bootstrap/Activation/Activation.jsx
+++ src/components/bootstrap/Activation/Activation.jsx
@@ -2,6 +2,9 @@ import React, { useRef } from "react";
 import { Button, Card, Container } from "react-bootstrap";
 import TermsSection from "./TermsSection";
 import { DialogHeader, DialogTitle } from "../../ui/Dialog";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { activateAccountMutationFn, getFilesQueryFn } from "../../../lib/api";
+import { toast } from "../../../hooks/use-toast";
 
 
 const Activation = ({
@@ -10,6 +13,48 @@ const Activation = ({
 }) => {
   const formRef = useRef();
 
+  const { data: uploadedFiles = [], isLoading: filesLoading } = useQuery({
+    queryKey: ['uploadedFiles'],
+    queryFn: getFilesQueryFn,
+
+  })
+
+  const { mutate: activateAccount, isPending } = useMutation({
+    mutationFn: activateAccountMutationFn,
+    onSuccess: (data) => {
+      toast({
+        title: "Activation Success!",
+        description: "Your account has been successfully activated! Please check your email for confirmation.",
+        variant: "success",
+      }),
+      console.log("Account successfully activated!", data);
+      formRef.current?.reset(); // Reset form after success
+      setTermsChecked(false); // Reset checkbox after success
+    },
+    onError: (error) => {
+      toast({
+        title: "Activation Failure",
+        description: `${error.message || "Activation failed. Please try again."}`,
+        variant: "destructive",
+      }),
+      console.error("Activation failed:", error);
+    }
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (uploadedFiles.length < 3) {
+      toast({
+        title: "Uploads Incomplete",
+        description: "Please upload at least 3 files to activate your account.",
+        variant: "desctructive",
+      });
+      return;
+    }
+
+    activateAccount({ termsChecked });
+  }
 
   return (
     <div className="bs">
@@ -32,23 +77,19 @@ const Activation = ({
               <Button
                 type="submit"
                 variant="secondary"
-                disabled={!termsChecked}
+                disabled={!termsChecked || isPending || filesLoading}
                 style={{
                   backgroundColor: "#c58c4f",
                   cursor: termsChecked ? "pointer" : "not-allowed",
-                  opacity: termsChecked ? 1 : 0.6,
+                  opacity: termsChecked && uploadedFiles.length >= 3 ? 1 : 0.6,
                   borderRadius: "5px",
                   padding: "5px 10px",
                   fontSize: "14px",
                   marginBottom: "20px",
                 }}
-                onClick={() =>
-                  formRef.current?.dispatchEvent(
-                    new Event("submit", { cancelable: true, bubbles: true })
-                  )
-                }
+                onClick={handleSubmit}
               >
-                Activate Now
+                {isPending ? "Activating...":  "Activate Now"}
               </Button>
             </div>
           </Card.Body>
