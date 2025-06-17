9ef5aca9271eb1d41246782fd80ff133bee5d65f
Essy116 <githinjie40@gmail.com>
2025-06-17 13:08:54 +0300

    "{CHORE} Import dialog component in payment instructions file"

--- src/components/workspace/activation/activate-now-dialog.jsx
+++ src/components/workspace/activation/activate-now-dialog.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { Dialog, DialogHeader, DialogTitle, DialogContent } from "../../ui/dialog";
+import {
+  Dialog,
+  DialogHeader,
+  DialogTitle,
+  DialogContent,
+} from "../../ui/dialog";
 import Activation from "../../bootstrap/Activation/Activation";
 import useActivateNowDialog from "../../../hooks/use-activate-now-dialog";
 
 const ActivateNowDialog = () => {
-    const { open, onClose } = useActivateNowDialog();
+  const { open, onClose } = useActivateNowDialog();
 
-    return (
-        <Dialog modal={true} open={open} onOpenChange={onClose}>
-            <DialogContent
-                className="w-full max-w-2xl border-0 !p-0 overflow-hidden"
-                style={{ height: "auto", maxHeight: "90vh" }}
-            >
-                <Activation />
-            </DialogContent>
-        </Dialog>
-    );
+  return (
+    <Dialog modal={true} open={open} onOpenChange={onClose}>
+      <DialogContent
+        className="w-full max-w-2xl border-0 !p-0 overflow-hidden"
+        style={{ height: "auto", maxHeight: "90vh" }}
+      >
+        <Activation />
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default ActivateNowDialog;
