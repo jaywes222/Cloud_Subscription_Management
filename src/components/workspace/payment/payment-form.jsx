6103f69eae30162005e412219b089ffd0f509ad0
Essy116 <githinjie40@gmail.com>
2025-06-19 00:52:33 +0300

    {REFACTOR} Replace OldPage import with NewPage in routing logic

--- src/components/workspace/payment/payment-form.jsx
+++ src/components/workspace/payment/payment-form.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { DialogHeader, DialogDescription, DialogTitle } from "../../ui/Dialog";
 import MpesaInstructions from "../../bootstrap/Payment/text/MpesaInstructions";
 
-import BankInstructions from "../../bootstrap/Payment/pay/BankInstructions";
+import BankConfirmation from "../../bootstrap/Payment/pay/BankConfirmation";
 const PaymentForm = () => {
   const [selectedOption, setSelectedOption] = useState("mpesa");
   const [showInstructions, setShowInstructions] = useState("mpesa");
@@ -19,36 +19,36 @@ const PaymentForm = () => {
 
   return (
     <div className="bs ">
-        <form>
-          <div className="radio-group">
-            <label className="custom-radio">
-              <input
-                type="radio"
-                name="paymentMethod"
-                value="mpesa"
-                checked={selectedOption === "mpesa"}
-                onClick={() => handleRadioClick("mpesa")}
-                readOnly
-              />
-              <span className="radio-label">MPESA</span>
-            </label>
-            {showInstructions === "mpesa" && <MpesaInstructions />}
+      <form>
+        <div className="radio-group">
+          <label className="custom-radio">
+            <input
+              type="radio"
+              name="paymentMethod"
+              value="mpesa"
+              checked={selectedOption === "mpesa"}
+              onClick={() => handleRadioClick("mpesa")}
+              readOnly
+            />
+            <span className="radio-label">MPESA</span>
+          </label>
+          {showInstructions === "mpesa" && <MpesaInstructions />}
 
-            <label className="custom-radio">
-              <input
-                type="radio"
-                name="paymentMethod"
-                value="bank"
-                checked={selectedOption === "bank"}
-                onClick={() => handleRadioClick("bank")}
-                readOnly
-              />
-              <span className="radio-label">BANK</span>
-            </label>
-            {showInstructions === "bank" && <BankInstructions />}
-          </div>
-        </form>
-      </div>
+          <label className="custom-radio">
+            <input
+              type="radio"
+              name="paymentMethod"
+              value="bank"
+              checked={selectedOption === "bank"}
+              onClick={() => handleRadioClick("bank")}
+              readOnly
+            />
+            <span className="radio-label">BANK</span>
+          </label>
+          {showInstructions === "bank" && <BankConfirmation />}
+        </div>
+      </form>
+    </div>
   );
 };
 
