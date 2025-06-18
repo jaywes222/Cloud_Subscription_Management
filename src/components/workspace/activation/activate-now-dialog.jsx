abea3bf59f1f233364fc6e77e8606a3dd3fc7445
Essy116 <githinjie40@gmail.com>
2025-06-19 00:53:04 +0300

    {REFACTOR} Eliminate redundant code for cleaner logic

--- src/components/workspace/activation/activate-now-dialog.jsx
+++ src/components/workspace/activation/activate-now-dialog.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Activation from "../../bootstrap/Activation/Activation";
 import useActivateNowDialog from "../../../hooks/use-activate-now-dialog";
-import Activation from "../../bootstrap/Activation/Activation";
+
 import { Dialog, DialogContent } from "../../ui/Dialog";
 
 const ActivateNowDialog = () => {
-    const { open, onClose } = useActivateNowDialog();
-    const [termsChecked, setTermsChecked] = useState(false);
+  const { open, onClose } = useActivateNowDialog();
+  const [termsChecked, setTermsChecked] = useState(false);
 
-    useEffect(() => {
-        if (!open) {
-            setTermsChecked(false); // Clear checkbox when dialog is closed
-        }
-    }, [open]);
+  useEffect(() => {
+    if (!open) {
+      setTermsChecked(false); // Clear checkbox when dialog is closed
+    }
+  }, [open]);
 
   return (
     <Dialog modal={true} open={open} onOpenChange={onClose}>
@@ -20,7 +20,10 @@ const ActivateNowDialog = () => {
         className="w-full max-w-2xl border-0 !p-0 overflow-hidden"
         style={{ height: "auto", maxHeight: "90vh" }}
       >
-        <Activation termsChecked={termsChecked} setTermsChecked={setTermsChecked}/>
+        <Activation
+          termsChecked={termsChecked}
+          setTermsChecked={setTermsChecked}
+        />
       </DialogContent>
     </Dialog>
   );
