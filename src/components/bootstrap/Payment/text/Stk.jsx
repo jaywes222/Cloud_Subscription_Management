86e3f7ab235c33f96f84859f50c8ecb00354b83e
Essy116 <githinjie40@gmail.com>
2025-06-19 00:53:49 +0300

    {FEAT} Add button and integrate React Bootstrap components

--- src/components/bootstrap/Payment/text/Stk.jsx
+++ src/components/bootstrap/Payment/text/Stk.jsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { Container, Card, ListGroup, Form, InputGroup } from "react-bootstrap";
+import {
+  Container,
+  Card,
+  ListGroup,
+  Form,
+  InputGroup,
+  Row,
+  Col,
+  Button,
+} from "react-bootstrap";
 
 const Stk = ({ phone, setPhone }) => {
   return (
     <div className="bs">
       <Container>
         <Card className="mpesa-card p-4">
-          <Card.Title className="mb-4 mpesa-card-title">
+          <Card.Title className=" mpesa-card-title">
             Follow the Steps Below. Once you receive a successful reply from
-            MPESA, click the button.
+            Mpesa, click the complete button below.
           </Card.Title>
 
-          <ListGroup as="ul" className="mpesa-steps-1 mb-4">
+          <ListGroup as="ul" className="mpesa-steps-1 mb-1">
             <ListGroup.Item as="li">
               Enter your Safaricom mobile phone number below and click{" "}
               <strong>Pay</strong>
@@ -24,8 +33,17 @@ const Stk = ({ phone, setPhone }) => {
               MPESA confirmation
             </ListGroup.Item>
           </ListGroup>
-
-          <Form className="  payment-form">
+          <Row className="mpesa-steps-3-footer py-1">
+            <Col xs="auto">
+              <Button
+                className="custom-complete-button"
+                onClick={() => navigate("/complete")}
+              >
+                Complete
+              </Button>
+            </Col>
+          </Row>
+          <Form className="  payment-form ">
             <Form.Group controlId="formPhoneNumber" className="w-100">
               <Form.Label>Phone number</Form.Label>
               <InputGroup className="w-100">
