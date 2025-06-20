aa4ea93163f226f720321b0691978a8e52df272d
Essy116 <githinjie40@gmail.com>
2025-06-20 16:54:23 +0300

    {REFACTOR} Update page structure and apply new styling

--- src/components/bootstrap/Payment/pay/BankConfirmation.jsx
+++ src/components/bootstrap/Payment/pay/BankConfirmation.jsx
@@ -20,77 +20,150 @@ const BankConfirmation = () => {
   return (
     <div className="bs">
       <Card className="mpesa-card p-4">
-        <Card.Title className="mb-4 mpesa-card-title text-center">
-          Follow the steps below. Once you have successfully made the bank
-          payment, click the Complete button below.
+        <Card.Title className="mb-4 mpesa-card-title ">
+          Once you have successfully made the payment,follow the steps below.
+          Then click the Confirm button .
         </Card.Title>
-        <Form>
-          <Form.Group controlId="payment-method" className="mb-1">
-            <Form.Label className="fw-semibold px-2">
-              Please Select Payment Mode
-            </Form.Label>
 
-            <Form.Select
-              aria-label="Select Payment Mode"
-              value={selectedMode}
-              onChange={handleChange}
+        <ListGroup as="ul" className="mpesa-steps-1 mb-1 mt-3">
+          <ListGroup.Item as="li">
+            Select your preferred <strong>Mode of Payment.</strong>
+          </ListGroup.Item>
+
+          <ListGroup.Item as="li">
+            Enter the following details in the fields below:
+            <ul className="centered-list">
+              <li>
+                <strong>Transaction ID/ Reference Number/ Cheque Number</strong>
+              </li>
+              <li>
+                <strong>Payment Amount</strong>
+              </li>
+              <li>
+                <strong>Payment Date</strong>
+              </li>
+            </ul>
+          </ListGroup.Item>
+          <ListGroup.Item as="li">
+            Once done please click <strong>'Confirm'</strong> button to proceed.
+          </ListGroup.Item>
+        </ListGroup>
+
+        <Form className="payment-form">
+          <Form.Group controlId="payment-method" className="mb-3">
+            <Form.Label
+              style={{
+                fontWeight: 500,
+
+                fontSize: "14px",
+                padding: "10px",
+                color: " #000000",
+                borderRadius: "10px",
+                margin: "20px 0",
+                minWidth: "100px",
+                width: "180px",
+                backgroundColor: "#dcb489",
+                display: "block",
+              }}
             >
-              <option value="Cash">Cash Deposit</option>
-              <option value="Cheque">Cheque Payment</option>
-              <option value="Bank Transfer">Mpesa Paybill</option>
-            </Form.Select>
+              Select Payment Mode
+            </Form.Label>
+            <div className=" w-100 mb-3">
+              <Form.Select
+                className="custom-dropdown-button"
+                value={selectedMode}
+                onChange={handleChange}
+                style={{
+                  fontWeight: 500,
+                  fontSize: "14px",
+                  border: "1px solid #a6a6a6 ",
+
+                  padding: "8px",
+                }}
+              >
+                <option value="Cash">Cash Deposit</option>
+                <option value="Cheque">Cheque Payment</option>
+                <option value="Bank Transfer">Mpesa Paybill</option>
+              </Form.Select>
+            </div>
           </Form.Group>
 
-          <ListGroup as="ul" className="mpesa-steps-1 mb-1 mt-3">
-            <ListGroup.Item as="li">
-              Locate your official <strong>bank payment receipt</strong> from
-              the teller
-            </ListGroup.Item>
-            <ListGroup.Item as="li">
-              Find the <strong>Transaction Reference Number</strong> on the
-              receipt (e.g. <code>EQ123456789</code>)
-            </ListGroup.Item>
-            <ListGroup.Item as="li">
-              Note the <strong>date of payment</strong> from the receipt
-            </ListGroup.Item>
-            <ListGroup.Item as="li">
-              You will enter the <strong>Reference Number</strong> and{" "}
-              <strong>Payment Date</strong> in the fields below
-            </ListGroup.Item>
-            <ListGroup.Item as="li">
-              Click the <strong>'Confirm'</strong> button
-            </ListGroup.Item>
-          </ListGroup>
           {selectedMode === "Cash" && (
-            <Form.Group controlId="formMpesaId">
-              <Form.Label>Transaction ID</Form.Label>
-              <Form.Control type="text" placeholder="Enter M-PESA ID" />
+            <Form.Group
+              controlId="formTransactionRef"
+              style={{
+                display: "inline-block",
+
+                minWidth: "500px",
+              }}
+            >
+              <InputGroup>
+                {" "}
+                <Form.Label>Reference Number</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter  Reference Number"
+                />
+              </InputGroup>
             </Form.Group>
           )}
-
           {selectedMode === "Cheque" && (
-            <Form.Group controlId="formChequeNumber">
-              <Form.Label>Cheque Number</Form.Label>
-              <Form.Control type="text" placeholder="Enter Cheque Number" />
+            <Form.Group
+              controlId="formChequeNumber"
+              style={{
+                display: "inline-block",
+
+                minWidth: "500px",
+              }}
+            >
+              <InputGroup>
+                <Form.Label>Cheque Number</Form.Label>
+                <Form.Control type="text" placeholder="Enter  Cheque Number" />
+              </InputGroup>
             </Form.Group>
           )}
 
           {selectedMode === "Bank Transfer" && (
-            <Form.Group controlId="formTransactionRef">
-              <Form.Label> Reference Number</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Mpesa Transaction Reference"
-              />
+            <Form.Group
+              controlId="formTransactionRef"
+              style={{
+                display: "inline-block",
+
+                minWidth: "420px",
+              }}
+            >
+              <InputGroup>
+                <Form.Label>Transaction ID</Form.Label>
+                <Form.Control type="text" placeholder="Enter  Transaction ID" />
+              </InputGroup>
             </Form.Group>
           )}
-          <Form.Group controlId="formTransactionDate">
-            <Form.Label> Payment Date</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter Mpesa Transaction Date"
-            />
-          </Form.Group>
+          <div className="mb-3">
+            <Form.Group
+              controlId="formAmount"
+              style={{
+                display: "inline-block",
+                marginRight: "1rem",
+                minWidth: "200px",
+              }}
+            >
+              <Form.Label>Amount</Form.Label>
+              <Form.Control type="number" placeholder="Enter Payment Amount" />
+            </Form.Group>
+
+            <Form.Group
+              controlId="formTransactionDate"
+              style={{
+                display: "inline-block",
+
+                minWidth: "200px",
+              }}
+            >
+              <Form.Label>Payment Date</Form.Label>
+              <Form.Control type="date" placeholder="Select Payment Date" />
+            </Form.Group>
+          </div>
+
           <Row className="mpesa-steps-1-footer py-2">
             <Col xs={12} md="auto" className="mb-1">
               <Button
---
+++ src/components/bootstrap/Payment/text/confirmation.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import {
+  Container,
+  Card,
+  ListGroup,
+  Button,
+  Row,
+  Col,
+  Form,
+  InputGroup,
+} from "react-bootstrap";
+
+const Confirmation = () => {
+  return (
+    <div className="bs">
+      <Card className="mpesa-card p-4">
+        <Card.Title className=" mpesa-card-title">
+          Follow the Steps Below. Once you receive a successful reply from
+          Mpesa, click the complete button below.
+        </Card.Title>
+
+        <ListGroup as="ul" className="mpesa-steps-1 mb-1">
+          <ListGroup.Item as="li">
+            Open the text message you received from <strong>MPESA</strong>
+          </ListGroup.Item>
+          <ListGroup.Item as="li">
+            Enter the <strong>MPESA Transaction ID</strong> below (e.g.{" "}
+            <code>RAT2G4W9QU</code>)
+          </ListGroup.Item>
+          <ListGroup.Item as="li">
+            Click the <strong>'Confirm'</strong> button
+          </ListGroup.Item>
+        </ListGroup>
+
+        <Form className="payment-form">
+          <Form.Group
+            controlId="formTransactionId"
+            style={{
+              display: "inline-block",
+              marginRight: "1rem",
+              minWidth: "350px",
+            }}
+          >
+            <Form.Label>Transaction ID</Form.Label>
+            <InputGroup>
+              <Form.Control
+                type="text"
+                placeholder="Enter Mpesa Transaction ID"
+                aria-label="MPESA Transaction ID"
+              />
+            </InputGroup>
+          </Form.Group>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default Confirmation;
